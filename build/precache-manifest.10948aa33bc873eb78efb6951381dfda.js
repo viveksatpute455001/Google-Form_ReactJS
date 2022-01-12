1a91e283b9f9cbb082b77b1d6e8c97b969ee22c5
Vivek Satpute <87656090+viveksatpute455001@users.noreply.github.com>
2022-01-12 22:21:21 +0530

    Add files via upload

---
+++ build/precache-manifest.10948aa33bc873eb78efb6951381dfda.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfee201343f4c2a6dbbfe3600c8a93f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35d1347cd480611b397",
+    "url": "/static/css/main.f4e96e8a.chunk.css"
+  },
+  {
+    "revision": "0e4e5f289dceeb740eee",
+    "url": "/static/js/2.afdde741.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.afdde741.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35d1347cd480611b397",
+    "url": "/static/js/main.94a49952.chunk.js"
+  },
+  {
+    "revision": "a06db7ae0e1a84380752",
+    "url": "/static/js/runtime-main.f3428da5.js"
+  },
+  {
+    "revision": "e8cc72ca67c4c8896c7dfac2ecae3c05",
+    "url": "/static/media/contact.e8cc72ca.png"
+  },
+  {
+    "revision": "0414379fd5426c2c5bf3e5e7fb169aaa",
+    "url": "/static/media/party_invite.0414379f.png"
+  },
+  {
+    "revision": "d07de1a1f6f25957b69040cc56b5c45a",
+    "url": "/static/media/t-shirt.d07de1a1.png"
+  }
+]);
